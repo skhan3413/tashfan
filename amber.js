b19ce564e50449fad683c7bb4819ed48e46a15cc
Sajid Khan <skhan3413@yahoo.com>
2017-05-08 02:31:36 +0000

    Make taimore tashfan program

---
+++ amber.js
@@ -0,0 +1,28 @@
+//console.log("tashfan");
+
+// Printing to the console tashfan 5 times
+
+
+ for (var i=0; i <= 3; i++)
+
+ {
+	console.log("tashfan");
+}
+if(i==3);
+else {
+console.log("taimore");
+}
+
+//for (var j=0; j<1;j=j+1) {  
+//	console.log("taimore");
+//	}
+
+
+
+//var i;
+//for (i=0; i < 100; i++) {
+//console.log ("welcome tashfan");
+//}
+
+
+
